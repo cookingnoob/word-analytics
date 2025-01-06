852e3a5abddd1b7e52282c3bf8419ac8a711894b
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2025-01-06 13:17:32 -0600

    add warning message component and logic for managing state for textarea and warning

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 const Textarea = () => {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState("");
+  const [warning, setWarning] = useState("");
+
+  const handleChange = (event) => {
+    let newText = event.target.value;
+    if (newText.includes("<script>")) {
+      newText = newText.replace("<script>", "");
+      setWarning("No script tags allowed!");
+    } else if (newText.includes("@")) {
+      newText = newText.replace("@", "");
+      setWarning("No @ allowed!");
+    }
+    setText(newText);
+  };
+
+  return (
+    <>
+      <textarea
+        value={text}
+        onChange={handleChange}
+        className="textarea"
+        placeholder="put your text here"
+      />
+      <Warning warning={warning} />
+    </>
+  );
 };
 
 export default Textarea;
